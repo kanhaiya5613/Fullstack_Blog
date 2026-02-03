8304ce2237fe08169ff03d13dd4aec04a89da1ba
kanhaiya kumar <kk7077574@gmail.com>
2026-02-04 01:27:11 +0530

    Add error handling and response utility classes; update dependencies

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,3 +6,8 @@ app.use(cors({
     origin:process.env.CORS.ORIGIN,
     credentials: true
 }))
+
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended: true, limit:"16kb"}))
+app.use(express.static("public"));
+app.use(cookieParser())
