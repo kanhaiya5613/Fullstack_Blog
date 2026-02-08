51cbe9681ad9afade543b1c72ea65cfb40141228
kanhaiya kumar <kk7077574@gmail.com>
2026-02-08 16:23:33 +0530

    Cors problem

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,29 +8,22 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://fullstack-blog-mauve.vercel.app/"
+  "https://fullstack-blog-mauve.vercel.app"
 ];
 
 app.use(cors({
-  origin: function(origin, callback) {
+  origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
+      console.log("Blocked by CORS:", origin);
       callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true,
 }));
 
-
-
-
-
-
-
-
-
-
+app.options("*", cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
